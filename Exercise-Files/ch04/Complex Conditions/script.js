1314a6569e70fe59ea0f68afb727eb0bb7835f2e
Camden_Shaw <camden.shaw@gmail.com>
2018-03-08 10:52:49 -0800

    Started Programmin Foundations: Real World Exampls - Code Reuse exercise.

---
+++ Exercise-Files/ch04/Complex Conditions/script.js
@@ -0,0 +1,10 @@
+var balance = 5000
+
+if (balance >= 0) {
+    alert("The balance is poitive")
+    if (balance > 10000) {
+        alert("the balance is large!")
+    }
+} else {
+    alert("The balance is negative")
+}
