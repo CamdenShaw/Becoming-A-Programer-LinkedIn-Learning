8787d21970580e6c838446337bcc5cffd6dc3a6d
Camden_Shaw <camden.shaw@gmail.com>
2018-03-09 10:31:52 -0800

    Finished exercise 'two names one shirt'

--- Programming-Foundations/Ch02/02_03/start_02_03_naming_shirts.py
+++ Programming-Foundations/Ch02/02_03/start_02_03_naming_shirts.py
@@ -10,3 +10,37 @@ class shirt:
 
     def make_clean(self):
         self.clean = True
+
+red = shirt()
+
+crimson = red
+
+print(id(red))
+print(id(crimson))
+
+print(red.clean)
+print(crimson.clean)
+
+red.make_dirty()
+
+print(red.clean)
+print(crimson.clean)
+
+print(red is crimson)
+
+crimson = shirt()
+
+print(id(red))
+print(id(crimson))
+
+print(crimson.clean)
+print(red.clean)
+
+red.make_clean()
+
+print(crimson.clean)
+print(red.clean)
+
+print(red is crimson)
+
+
