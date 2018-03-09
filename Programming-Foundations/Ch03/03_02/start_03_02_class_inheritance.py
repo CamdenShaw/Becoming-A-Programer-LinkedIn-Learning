84473d3d411b12006f34d5c04fd255a8399a5ada
Camden_Shaw <camden.shaw@gmail.com>
2018-03-09 11:54:45 -0800

    Finished 'Override Inherited Methods' exercise.

--- Programming-Foundations/Ch03/03_02/start_03_02_class_inheritance.py
+++ Programming-Foundations/Ch03/03_02/start_03_02_class_inheritance.py
@@ -29,3 +29,34 @@ class Motorcycle(Vehicle): # Inherits from Vehicle class
     # put on motocycle helmet
     def helmet(self):
         print('Nice and safe!')
+
+class ECar(Car):
+    def drive(self):
+        print('The {} {} goes shhhhhhhh...' .format(self.color, self.manuf))
+
+my_ecar = ECar('white', 'Nissan')
+my_ecar.radio()
+my_ecar.window()
+my_ecar.drive()
+print(my_ecar.gas)
+
+""" My Experiments with overriding methods """
+
+class eCar(Car):
+    Car.power = 4
+    
+    def drive(self):
+        if self.gas > 0:
+            self.gas = 0
+        if self.power > 0:
+            self.power -= 1
+            print('The {} {} goes shhhhhhhh...' .format(self.color, self.manuf))
+        else:
+            print('The {} {} comes to a quiet stop.' .format(self.color, self.manuf))
+
+my_eCar = eCar('white', 'Nissan')
+my_eCar.radio()
+my_eCar.window()
+my_eCar.drive()
+print(my_eCar.gas)
+print(my_eCar.power)
