8219aa73440ca5418732473b7eab3b442ab0eb30
Camden_Shaw <camden.shaw@gmail.com>
2018-03-09 09:23:56 -0800

    Completed code reuse exercise and experimented with python on my own.

--- Programming-Foundations/Ch01/01_02/start_01_02_breakfast_functions.py
+++ Programming-Foundations/Ch01/01_02/start_01_02_breakfast_functions.py
@@ -1,19 +1,36 @@
 """ A Functional Breakfast """
-
-def make_omelette():
+""" As per instructions """
+def mix_and_cook():
     print('Mixing the ingredients')
     print('Pouring the mixture into a frying pan')
     print('Cooking the first side')
     print('Flipping it!')
     print('Cooking the other side')
+
+def make_omelette():
+    mix_and_cook()
     omelette = 'a tasty omelette'
     return omelette
 
 def make_pancake():
+    mix_and_cook()
+    pancake = 'a tasty pancake'
+    return pancake
+
+""" Figuring out variables in functions on my own """
+
+def make_dish(x):
     print('Mixing the ingredients')
     print('Pouring the mixture into a frying pan')
     print('Cooking the first side')
     print('Flipping it!')
     print('Cooking the other side')
-    pancake = 'a delicious pancake'
-    return pancake
+    y = 'a tasty ' + x
+    print(y)
+    return y
+
+def make_omelette():
+    mix_and_cook('omelette')
+
+def make_pancake():
+    mix_and_cook('pancake')
