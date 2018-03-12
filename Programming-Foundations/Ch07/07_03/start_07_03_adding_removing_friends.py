03604cd9c298deb05de120d033d473ab10ebc16f
Camden_Shaw <camden.shaw@gmail.com>
2018-03-12 12:53:33 -0700

    Finished 'Add and Remove Items From Sets' exercise from Chapter 7.

--- Programming-Foundations/Ch07/07_03/start_07_03_adding_removing_friends.py
+++ Programming-Foundations/Ch07/07_03/start_07_03_adding_removing_friends.py
@@ -2,3 +2,18 @@
 
 # revised set of friends to invite
 invite = set(['Nestor', 'Amanda', 'Olivia'])
+
+print('Verne' in invite)
+
+invite.add('Verne')
+invite.add('Olivia')
+print(invite)
+
+invite.remove('Nestor')
+print(invite)
+# invite.remove('Nestor') 
+
+print(invite.pop())
+print(invite.pop())
+print(invite.pop())
+print(invite.pop()) # error
