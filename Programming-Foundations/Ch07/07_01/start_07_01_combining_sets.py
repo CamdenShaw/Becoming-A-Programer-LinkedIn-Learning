9f81e89553ffb131bbcab0c54a2cbce267abfdad
Camden_Shaw <camden.shaw@gmail.com>
2018-03-12 10:48:02 -0700

    Finished 'Create and Combine Sets' exercise of Chapter 7

--- Programming-Foundations/Ch07/07_01/start_07_01_combining_sets.py
+++ Programming-Foundations/Ch07/07_01/start_07_01_combining_sets.py
@@ -8,3 +8,24 @@ coworker = set(['Aaron', 'Bill', 'Brandon', 'David',
 
 family = set(['Garry', 'Landon', 'Larry', 'Mark',
               'Olivia', 'Katy', 'Rae', 'Tom'])
+
+print(college)
+print(len(college))
+print(len(coworker))
+print(len(family))
+
+friends = college.union(coworker, family)
+print(friends)
+print(len(friends))
+
+print('I have {} college buddies:'.format(len(college)))
+print(college)
+
+print('I have {} coworkers:'.format(len(college)))
+print(coworker)
+
+print('I have {} family friends:'.format(len(family)))
+print(family)
+
+print('I have {} total friends:'.format(len(friends)))
+print(friends)
