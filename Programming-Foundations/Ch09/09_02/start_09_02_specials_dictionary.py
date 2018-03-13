3c7f4c277a59a1e9d6d3430a1a67c08e50b472d4
Camden_Shaw <camden.shaw@gmail.com>
2018-03-13 11:16:25 -0700

    Finshed the 'Switch and Case Statements' exercise from Chapter 9.  Experimented with the logic.

--- Programming-Foundations/Ch09/09_02/start_09_02_specials_dictionary.py
+++ Programming-Foundations/Ch09/09_02/start_09_02_specials_dictionary.py
@@ -1,4 +1,5 @@
 """ I'll Have the Special! """
+from datetime import date
 
 specials = {'Sunday'    : 'spinach', 
             'Monday'    : 'mushroom', 
@@ -11,3 +12,15 @@ specials = {'Sunday'    : 'spinach',
 def order(day):
     pizza = specials[day]
     print('Order the {} pizza.'.format(pizza))
+
+print(order('Saturday'))
+
+#Experiments
+# making the program less reliant on user input
+weekday = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+today = weekday[date.today().weekday()]
+print(order(today))
+
+def order(day = today):
+    pizza = specials[day]
+    print('Order the {} pizza'.format(pizza))
