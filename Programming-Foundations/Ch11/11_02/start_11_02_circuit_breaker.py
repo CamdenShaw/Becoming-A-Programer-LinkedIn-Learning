92e24885290087dca272058f004651e5a9ed30de
Camden_Shaw <camden.shaw@gmail.com>
2018-03-15 11:10:56 -0700

    Finished 'Validate External Input' exercise from chapter 11.

--- Programming-Foundations/Ch11/11_02/start_11_02_circuit_breaker.py
+++ Programming-Foundations/Ch11/11_02/start_11_02_circuit_breaker.py
@@ -7,7 +7,38 @@ class CircuitBreaker:
         self.load = 0            # present load in amps
 
     def connect(self, amps):
-        self.load += amps
+        if self.load + amps > self.capacity:
+            raise Exception('Connection will exceed capacity.')
+        elif self.load + amps < 0:
+            raise Exception('Connection cannot generate a negative load.')
+        else:
+            self.load += amps
 
 # create a 20A circuit breaker
 cb = CircuitBreaker(20)
+
+print(cb.load)
+
+try:
+    cb.connect(12)
+except Exception as e:
+    print(e)
+print(cb.load)
+
+try:
+    cb.connect(7)
+except Exception as e:
+    print(e)
+print(cb.load)
+    
+try :
+    cb.connect(10)
+except Exception as e:
+    print(e)
+print(cb.load)
+
+try:
+    cb.connect(-20)
+except Exception as e:
+    print(e)
+print(cb.load)
